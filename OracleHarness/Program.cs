12134ce14517546ad89126d21b5b6d97124ea357
venkat.kannan <venkat.kannan@gmail.com>
2014-11-09 22:21:10 +0000

    Harness to check Oracle memory leak

--- OracleHarness/Program.cs
+++ OracleHarness/Program.cs
@@ -10,15 +10,15 @@ namespace OracleHarness
     class Program
     {
         private static string connectionString = string.Format(
-"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={0})(PORT=1521))(CONNECT_DATA=(SERVICE_NAME={1})));User Id={2};Password={3};Pooling=true;",
+"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={0})(PORT=1521))(CONNECT_DATA=(SERVICE_NAME={1})));User Id={2};Password={3};Pooling=true;Max Pool Size=250",
 "localhost", "xe", "cs_scheduler", "tesco");
         static void Main(string[] args)
         {
             var tasks = new List<Task>();
-            for (int i = 0; i < 128; i++)
+            for (int i = 0; i < 64; i++)
             {
                 int index = i;
-                var task = Task.Factory.StartNew(() => OracleQueueTest(index));
+                var task = Task.Factory.StartNew(() => OracleQueueTest(index), TaskCreationOptions.LongRunning);
                 tasks.Add(task);
             }
             Task.WaitAll(tasks.ToArray());
@@ -26,46 +26,56 @@ namespace OracleHarness
 
         private static void OracleQueueTest(int identifier)
         {
-            int index = 0;
-            while (true)
+            try
             {
-                var connection = new OracleConnection(connectionString);
-                var queue = new OracleAQQueue("SCHEDULE_QUEUE", connection)
+                int index = 0;
+                while (true)
                 {
-                    MessageType = OracleAQMessageType.Raw,
-                    EnqueueOptions =
+                    var connection = new OracleConnection(connectionString);
+                    var queue = new OracleAQQueue("SCHEDULE_QUEUE", connection)
+                    {
+                        MessageType = OracleAQMessageType.Raw,
+                        EnqueueOptions =
                     {
                         Visibility = OracleAQVisibilityMode.OnCommit,
                         DeliveryMode = OracleAQMessageDeliveryMode.Persistent
                     }
-                };
-                connection.Open();
-                var transaction = connection.BeginTransaction();
+                    };
+                    connection.Open();
+                    var transaction = connection.BeginTransaction();
 
-                var dequeueOptions = new OracleAQDequeueOptions
-                {
-                    Visibility = OracleAQVisibilityMode.OnCommit,
-                    NavigationMode = OracleAQNavigationMode.NextMessage,
-                    DequeueMode = OracleAQDequeueMode.Remove,
-                    DeliveryMode = OracleAQMessageDeliveryMode.Persistent,
-                    Wait = 1
-                };
-                try
-                {
-                    queue.Dequeue(dequeueOptions);
-                }
-                catch
-                {
-                }
+                    var dequeueOptions = new OracleAQDequeueOptions
+                    {
+                        Visibility = OracleAQVisibilityMode.OnCommit,
+                        NavigationMode = OracleAQNavigationMode.NextMessage,
+                        DequeueMode = OracleAQDequeueMode.Remove,
+                        DeliveryMode = OracleAQMessageDeliveryMode.Persistent,
+                        Wait = 1
+                    };
+                    try
+                    {
+                        queue.Dequeue(dequeueOptions);
+                    }
+                    catch
+                    {
+                    }
 
-                transaction.Commit();
-                transaction.Dispose();
-                queue.Dispose();
-                connection.Dispose();
-                connection.Close();
-                ++index;
-                Console.WriteLine("[{0:D2}] Iteration {1:D4}", identifier, index);
+                    transaction.Commit();
+                    transaction.Dispose();
+                    queue.Dispose();
+                    connection.Dispose();
+                    connection.Close();
+                    ++index;
+                    Console.WriteLine("[{0:D2}] Iteration {1:D4}", identifier, index);
+                }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+           
         }
     }
 }
