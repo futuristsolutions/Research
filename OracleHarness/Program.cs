4b096aba21ab29100b1bea8a4907c2c9815f947d
venkat.kannan <venkat.kannan@gmail.com>
2014-11-10 23:33:54 +0000

    Harness to check Oracle memory leak

--- OracleHarness/Program.cs
+++ OracleHarness/Program.cs
@@ -9,32 +9,54 @@ namespace OracleHarness
 {
     class Program
     {
-        private static string connectionString = string.Format(
-"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={0})(PORT=1521))(CONNECT_DATA=(SERVICE_NAME={1})));User Id={2};Password={3};Pooling=true;Max Pool Size=250",
+        private static readonly string ConnectionString = string.Format(
+"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={0})(PORT=1521))(CONNECT_DATA=(SERVICE_NAME={1})));User Id={2};Password={3};Pooling=true",
 "localhost", "xe", "cs_scheduler", "tesco");
         static void Main(string[] args)
         {
-            var tasks = new List<Task>();
-            for (int i = 0; i < 64; i++)
+            int algo = 1;
+            if (args.Any())
             {
-                int index = i;
-                var task = Task.Factory.StartNew(() => OracleQueueTestUsingListen(index), TaskCreationOptions.LongRunning);
-                tasks.Add(task);
+                var parts = args.First().Split(new[] {'='});
+                if (parts.First().Contains("dq"))
+                {
+                    algo = Convert.ToInt32(parts.Last());
+                }
+                else
+                {
+                    algo = -1;
+                }
             }
-            Task.WaitAll(tasks.ToArray());
+
+            if (algo != -1)
+            {
+                var action = (algo == 1)
+                    ? (Action<int>)(DequeueTestUsingListen)
+                    : DequeueTestNoListen;
+                var tasks = new List<Task>();
+                for (int index = 1; index <= 64; index++)
+                {
+                    int identifier = index;
+                    var task = Task.Factory.StartNew(() => action(identifier), TaskCreationOptions.LongRunning);
+                    tasks.Add(task);
+                }
+
+                Console.WriteLine("{0} - {1}", action.Method.Name, tasks.Count);
+                Task.WaitAll(tasks.ToArray());
+            }
         }
 
-        private static void OracleQueueTestUsingListen(int identifier)
+        private static void DequeueTestUsingListen(int identifier)
         {
             try
             {
-                var aqueueAgent = new List<OracleAQAgent>() {new OracleAQAgent(null,"CS_SCHEDULER.SCHEDULE_QUEUE")};
+                var aqueueAgent = new [] {new OracleAQAgent(null,"CS_SCHEDULER.SCHEDULE_QUEUE")};
                 int index = 0;
-                var connection = new OracleConnection(connectionString);
+                var connection = new OracleConnection(ConnectionString);
                 connection.Open();
                 while (true)
                 {
-                    var agent = OracleAQQueue.Listen(connection, aqueueAgent.ToArray(), 1);
+                    var agent = OracleAQQueue.Listen(connection, aqueueAgent, 1);
                     if (agent != null)
                     {
                         var queue = new OracleAQQueue("SCHEDULE_QUEUE", connection)
@@ -68,28 +90,27 @@ namespace OracleHarness
                         queue.Dispose();
                     }
                     ++index;
-                    Console.WriteLine("[{0:D2}] Iteration {1:D4}", identifier, index);
+                    //Console.WriteLine("[{0:D2}] Iteration {1:D4}", identifier, index);
                 }
-                connection.Close();
-                connection.Dispose();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
+                Console.WriteLine("Identifier : {0}", identifier);
                 Environment.Exit(0);
             }
            
         }
 
-        private static void OracleQueueTest(int identifier)
+        private static void DequeueTestNoListen(int identifier)
         {
             try
             {
                 int index = 0;
                 while (true)
                 {
-                    var connection = new OracleConnection(connectionString);
+                    var connection = new OracleConnection(ConnectionString);
                     var queue = new OracleAQQueue("SCHEDULE_QUEUE", connection)
                     {
                         MessageType = OracleAQMessageType.Raw,
@@ -124,7 +145,7 @@ namespace OracleHarness
                     connection.Dispose();
                     connection.Close();
                     ++index;
-                    Console.WriteLine("[{0:D2}] Iteration {1:D4}", identifier, index);
+                    //Console.WriteLine("[{0:D2}] Iteration {1:D4}", identifier, index);
                 }
             }
             catch (Exception e)
