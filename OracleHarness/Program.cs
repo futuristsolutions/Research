68eca29cb89f4cd4685f13b519aeb847750d4f4d
Venkat Kannan <venkat.kannan@uk.tesco.com>
2014-11-11 16:43:14 +0000

    Potential fix for memory leak issue.

--- OracleHarness/Program.cs
+++ OracleHarness/Program.cs
@@ -12,15 +12,25 @@ namespace OracleHarness
         private static readonly string ConnectionString = string.Format(
 "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={0})(PORT=1521))(CONNECT_DATA=(SERVICE_NAME={1})));User Id={2};Password={3};Pooling=true",
 "localhost", "xe", "cs_scheduler", "tesco");
+        private const int NoMessagesErrorCode = 25228;
         static void Main(string[] args)
         {
             int algo = 1;
+            int delay = 0;
             if (args.Any())
             {
                 var parts = args.First().Split(new[] {'='});
                 if (parts.First().Contains("dq"))
                 {
                     algo = Convert.ToInt32(parts.Last());
+                }
+                else if (parts.First().Contains("eq"))
+                {
+                    algo = -1;
+                    if (parts.Count() == 2)
+                    {
+                        delay = Convert.ToInt32(parts.Last());
+                    }
                 }
                 else
                 {
@@ -30,19 +40,60 @@ namespace OracleHarness
 
             if (algo != -1)
             {
-                var action = (algo == 1)
-                    ? (Action<int>)(DequeueTest_UsingListen)
-                    : DequeueTest_NoListen;
-                var tasks = new List<Task>();
-                for (int index = 1; index <= 32; index++)
-                {
-                    int identifier = index;
-                    var task = Task.Factory.StartNew(() => action(identifier), TaskCreationOptions.LongRunning);
-                    tasks.Add(task);
-                }
+                DequeueTest(algo);
+            }
+            else
+            {
+                EnqueueData(delay);
+            }
+        }
+        
+        private static void DequeueTest(int algo)
+        {
+            var action = (algo == 1)
+                ? (Action<int>)(DequeueTest_UsingListen)
+                : DequeueTest_NoListen;
+            var tasks = new List<Task>();
+            for (int index = 1; index <= 32; index++)
+            {
+                int identifier = index;
+                var task = Task.Factory.StartNew(() => action(identifier), TaskCreationOptions.LongRunning);
+                tasks.Add(task);
+            }
 
-                Console.WriteLine("{0} - {1}", action.Method.Name, tasks.Count);
-                Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("{0} - {1}", action.Method.Name, tasks.Count);
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private static void EnqueueData(int delay)
+        {
+            Console.WriteLine("Message delay seconds {0}",delay);
+            var connection = new OracleConnection(ConnectionString);
+            connection.Open();
+            for (int index = 1; index <= 5; index++)
+            {
+                var data = string.Format("Test message : {0} - {1}", Guid.NewGuid().ToString(), index);
+                var message = new OracleAQMessage
+                {
+
+                    Payload = Encoding.UTF8.GetBytes(data),
+                    Correlation = index.ToString(),
+                    Delay = 0
+                };
+
+                var queue = new OracleAQQueue("SCHEDULE_QUEUE", connection)
+                {
+                    MessageType = OracleAQMessageType.Raw,
+                    EnqueueOptions =
+                    {
+                        Visibility = OracleAQVisibilityMode.OnCommit,
+                        DeliveryMode = OracleAQMessageDeliveryMode.Persistent
+                    }
+                };
+                Console.WriteLine("Enqueueing {0}",data);
+                var transaction = connection.BeginTransaction();
+                queue.Enqueue(message);
+                transaction.Commit();
             }
         }
 
@@ -59,35 +110,7 @@ namespace OracleHarness
                     var agent = OracleAQQueue.Listen(connection, aqueueAgent, 1);
                     if (agent != null)
                     {
-                        var queue = new OracleAQQueue("SCHEDULE_QUEUE", connection)
-                        {
-                            MessageType = OracleAQMessageType.Raw,
-                            EnqueueOptions =
-                            {
-                                Visibility = OracleAQVisibilityMode.OnCommit,
-                                DeliveryMode = OracleAQMessageDeliveryMode.Persistent
-                            }
-                        };
-                        var transaction = connection.BeginTransaction();
-                        var dequeueOptions = new OracleAQDequeueOptions
-                        {
-                            Visibility = OracleAQVisibilityMode.OnCommit,
-                            NavigationMode = OracleAQNavigationMode.NextMessage,
-                            DequeueMode = OracleAQDequeueMode.Remove,
-                            DeliveryMode = OracleAQMessageDeliveryMode.Persistent,
-                            Wait = 1
-                        };
-                        try
-                        {
-                            queue.Dequeue(dequeueOptions);
-                        }
-                        catch
-                        {
-                        }
-                        transaction.Commit();
-                        queue.Dispose();
-                        transaction.Dispose();
-                        queue.Dispose();
+                        Dequeue(connection);
                     }
                     ++index;
                     //Console.WriteLine("[{0:D2}] Iteration {1:D4}", identifier, index);
@@ -108,42 +131,11 @@ namespace OracleHarness
             try
             {
                 int index = 0;
+                var connection = new OracleConnection(ConnectionString);
+                connection.Open();
                 while (true)
                 {
-                    var connection = new OracleConnection(ConnectionString);
-                    var queue = new OracleAQQueue("SCHEDULE_QUEUE", connection)
-                    {
-                        MessageType = OracleAQMessageType.Raw,
-                        EnqueueOptions =
-                        {
-                            Visibility = OracleAQVisibilityMode.OnCommit,
-                            DeliveryMode = OracleAQMessageDeliveryMode.Persistent
-                        }
-                    };
-                    connection.Open();
-                    var transaction = connection.BeginTransaction();
-
-                    var dequeueOptions = new OracleAQDequeueOptions
-                    {
-                        Visibility = OracleAQVisibilityMode.OnCommit,
-                        NavigationMode = OracleAQNavigationMode.NextMessage,
-                        DequeueMode = OracleAQDequeueMode.Remove,
-                        DeliveryMode = OracleAQMessageDeliveryMode.Persistent,
-                        Wait = 1
-                    };
-                    try
-                    {
-                        queue.Dequeue(dequeueOptions);
-                    }
-                    catch
-                    {
-                    }
-
-                    transaction.Commit();
-                    transaction.Dispose();
-                    queue.Dispose();
-                    connection.Dispose();
-                    connection.Close();
+                    Dequeue(connection);
                     ++index;
                     //Console.WriteLine("[{0:D2}] Iteration {1:D4}", identifier, index);
                 }
@@ -155,6 +147,46 @@ namespace OracleHarness
                 throw;
             }
 
+        }
+
+
+        private static void Dequeue(OracleConnection connection)
+        {
+            var queue = new OracleAQQueue("SCHEDULE_QUEUE", connection)
+            {
+                MessageType = OracleAQMessageType.Raw,
+                EnqueueOptions =
+                {
+                    Visibility = OracleAQVisibilityMode.OnCommit,
+                    DeliveryMode = OracleAQMessageDeliveryMode.Persistent
+                }
+            };
+            var transaction = connection.BeginTransaction();
+            var dequeueOptions = new OracleAQDequeueOptions
+            {
+                Visibility = OracleAQVisibilityMode.OnCommit,
+                NavigationMode = OracleAQNavigationMode.NextMessage,
+                DequeueMode = OracleAQDequeueMode.Remove,
+                DeliveryMode = OracleAQMessageDeliveryMode.Persistent,
+                Wait = 1
+            };
+            try
+            {
+                var message = queue.Dequeue(dequeueOptions);
+                if (message != null)
+                {
+                    var data = Encoding.UTF8.GetString((byte[])message.Payload);
+                    Console.WriteLine(data);
+                }
+            }
+            catch (OracleException ex)
+            {
+                if (ex.Number != NoMessagesErrorCode)
+                    throw;
+            }
+            transaction.Commit();
+            transaction.Dispose();
+            queue.Dispose();
         }
     }
 }
