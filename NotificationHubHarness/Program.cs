59b3b4352f9cc9af9ba05732d0fcf7980991afeb
venkat.kannan <venkat.kannan@gmail.com>
2014-11-19 22:13:42 +0000

    Play with notification hubs

---
+++ NotificationHubHarness/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Notifications;
+
+namespace NotificationHubHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var registrations = GetAllRegisteredDevicesAsync("Endpoint=sb://contactservice-demo.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=EZA5Q88Tt9XjdxCi8GgePjBc+9Ah+Xeg1Zol+f+ul4k=",
+                "contactservice-demo-hub");
+
+            int index = 0;
+            foreach (var registration in registrations)
+            {
+                Console.WriteLine("{0}",++index);
+                Console.WriteLine(registration.RegistrationId);
+                if (registration is GcmRegistrationDescription)
+                {
+                    Console.WriteLine((registration as GcmRegistrationDescription).GcmRegistrationId);
+                }
+                if (registration is AppleRegistrationDescription)
+                {
+                    Console.WriteLine((registration as AppleRegistrationDescription).DeviceToken);
+                }
+                    foreach (var tag in registration.Tags)
+                {
+                    Console.WriteLine(tag);
+                }
+            }
+        }
+
+        internal static List<RegistrationDescription> GetAllRegisteredDevicesAsync(string connectionString,
+                string notificationPath)
+        {
+            var hub = NotificationHubClient.CreateClientFromConnectionString(connectionString, notificationPath);
+
+            var allRegistrations = hub.GetAllRegistrationsAsync(0).Result;
+            var continuationToken = allRegistrations.ContinuationToken;
+            var registrationDescriptionsList = new List<RegistrationDescription>(allRegistrations);
+            while (!string.IsNullOrWhiteSpace(continuationToken))
+            {
+                var otherRegistrations = hub.GetAllRegistrationsAsync(continuationToken, 0).Result;
+                registrationDescriptionsList.AddRange(otherRegistrations);
+                continuationToken = otherRegistrations.ContinuationToken;
+            }
+
+            return registrationDescriptionsList;
+        }
+    }
+}
