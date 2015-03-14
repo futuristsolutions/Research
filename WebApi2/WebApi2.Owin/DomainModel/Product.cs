4af72510f6fff3627b5d212419c9a5d248c0269f
venkat.kannan <venkat.kannan@gmail.com>
2015-03-14 18:30:52 +0000

    WebApi2 using Owin

---
+++ WebApi2/WebApi2.Owin/DomainModel/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi2.Owin.DomainModel
+{
+    public class Product
+    {
+        public int? Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ WebApi2/WebApi2.Owin/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApi2.Owin.Startup))]
+
+namespace WebApi2.Owin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.Routes.MapHttpRoute(
+             name: "DefaultApi",
+             routeTemplate: "api/{controller}/{id}",
+             defaults: new { id = RouteParameter.Optional });
+            app.UseWebApi(httpConfiguration);
+
+        }
+    }
+}
